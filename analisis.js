f9d2668cbb26a544b5bbf1d9a739afc8e1ce23ea
AndriwRC <randriw4@gmail.com>
2021-08-10 21:26:10 -0500

    Taller 4: analisis salarial

---
+++ analisis.js
@@ -0,0 +1,58 @@
+// Utils
+function calcularMediaAritemtica(lista) {
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+}
+
+function esPar(numero) {
+    return (numero % 2 === 0);
+}
+
+// Calculadora de mediana
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length /  2);
+
+    if (esPar(lista.length)) {
+        const personaMitad1 = lista[mitad - 1];
+        const personaMitad2 = lista[mitad];
+
+        const mediana = calcularMediaAritemtica([personaMitad1, personaMitad2]);
+        return mediana;
+    } else {
+        const mediana = lista[mitad];
+        return mediana;
+    }
+}
+
+// Mediana General
+const salariosCol = colombia.map(
+    function (persona) {
+        return persona.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort((salaryA, salaryB) => salaryA - salaryB);
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+// Mediana del top 10%
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosTop10Col = salariosColSorted.splice(
+    spliceStart,
+    spliceCount,
+);
+
+const medianaTop10Col = medianaSalarios(salariosTop10Col);
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+});
